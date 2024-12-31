30d700525cc1549332492c641b47953c953c5356
Sumit Kumar Sahu <sumitkumarsahu@Sumits-MacBook-Air.local>
2025-01-01 03:03:33 +0530

    Exception handling in SpringBoot

--- src/main/java/com/example/springbootlearning/ExceptionHanding/CustomLevelTwoException.java
+++ src/main/java/com/example/springbootlearning/ExceptionHanding/CustomLevelTwoException.java
@@ -4,9 +4,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 /**
+ * This is the custom exception class annotated with @ResponseStatus
+ * which means if any CustomLevelTwoException uncaught by the ExceptionHandlerExceptionResolver then
+ *
+ * it goes to ResponseStatusExceptionResolver it check the CustomLevelTwoException annotated with @ResponseStatus
+ * then it will resolve this exception and send the response
  *
  */
-
 
 @ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Invalid request")
 public class CustomLevelTwoException extends RuntimeException{
